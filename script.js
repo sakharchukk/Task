6089a8a8ac79b895eef4051d908dbabf4002c314
Dmytro <sakharchukcoder@gmail.com>
2020-01-30 15:29:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+  var randomNum = Math.floor((Math.random() * 1000) + 8888);
+  var el = document.getElementById('ranNum');
+  el.innerHTML = randomNum;
+
+  function showText() {
+    alert ("Спасибо");
+  }
